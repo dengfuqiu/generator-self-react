4621189b6c7bf7b2d776c671065d663f5d68119c
dengfuqiu <kmdengfuqiu@126.com>
2017-05-31 13:56:45 +0800

    删除无用的代码

--- app.js
+++ app.js
@@ -2,18 +2,10 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component {
-  test(...a) {
-    console.log(a);
-  }
-
   render() {
-    const a = 1;
-    const b = 2;
-    const c = 3;
-    this.test(a, b, c);
     return (
       <div>
-        this is app aaaaa
+        this is app
       </div>
     );
   }
